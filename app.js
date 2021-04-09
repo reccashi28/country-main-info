9fbc98fe4e4d2ed3bc14cf8068983144bbf44fd1
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-04-09 18:48:17 +0200

    updated code to es6

--- app.js
+++ app.js
@@ -3,47 +3,47 @@ const inputText = document.querySelector('.country-input');
 const searchBtn = document.querySelector('.country-btn');;
 
 
-//create request
-
-    function getCountry(country){
-        let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function(){
-                if(this.readyState === 4 && this.status === 200 ){
-                    xhttp.addEventListener('load', () => {
-                        //JSON string to JS object
-                        let [data] = JSON.parse(xhttp.responseText);
-                        console.log(data);
-                        const html = `
-                        <article class="country">
-                            <div class="country-img">
-                                <img src="${data.flag}" />
-                            </div>
-                            
-                            <div class="country-data">
-                                <h3 class="country-name"> ${data.name} </h3>
-                                <h4 class="country-region"><span>Region: </span> ${data.region} </h4>
-                                <p class="country-row"><span><i class="fas fa-users"></i> Population: </span> ${data.population} </p>
-                                <p class="country-row"><span><i class="far fa-smile"></i> Languages: </span> ${data.languages[0].name} </p>
-                                <p class="country-row"><span><i class="far fa-money-bill-alt"></i> Currencies: </span> ${data.currencies[0].name} </p>
-                            </div>
-                        </article>
-                        `;
-
-                        countries.insertAdjacentHTML('afterbegin', html);
-                    });
-                } 
-            }
-        
-        xhttp.open('GET', 'https://restcountries.eu/rest/v2/name/'+country, true);
-        xhttp.send();  
+//fetch data
+async function fetchCountries() {
+    try {
+        const response = await fetch('https://restcountries.eu/rest/v2/all');
+        const data = await response.json();
+        return data;
+    } catch(e) {
+        console.log('ERROR OCCURED',e)
     }
+}
+
+async function getCountriesList(searchField) {
+    const data = await fetchCountries();
+    const countriesList = data.filter ( country => {
+        if(searchField){
+            return country.name.toLowerCase().includes(searchField.toLowerCase());
+        } else {
+            return country;
+        }
+        
+    })
+
+    const html = countriesList.map( country => {
+                return `<section class="country">
+                    <div class="country-img">
+                        <img src="${country.flag}" />
+                    </div>
+                    <div class="country-data">
+                        <h3 class="country-name"> ${country.name} </h3>
+                        <h4 class="country-region"><span>Region: </span> ${country.region} </h4>
+                        <p class="country-row"><span><i class="fas fa-users"></i> Population: </span> ${country.population} </p>
+                        <p class="country-row"><span><i class="far fa-smile"></i> Languages: </span> ${country.languages[0].name} </p>
+                        <p class="country-row"><span><i class="far fa-money-bill-alt"></i> Currencies: </span> ${country.currencies[0].name} </p>
+                    </div>
+                </section> `;
+        })
+        countries.innerHTML= html.map( list => list).join("");
+}
 
 searchBtn.addEventListener('click', () => {
-    getCountry(inputText.value);
+    getCountriesList(inputText.value)
     inputText.value = "";
-});
-    
-
-getCountry("usa");
-getCountry("germany");
-getCountry("Philippines");
+});    
+getCountriesList();
